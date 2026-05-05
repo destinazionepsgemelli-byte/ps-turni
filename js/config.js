c873b94c2bf8289a8125af5c5dd2ea97c306039d
destinazionepsgemelli-byte <destinazionepsgemelli@gmail.com>
2026-05-05 12:54:43 +0200

    .

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://hqgdnhinxvaoeicplkbj.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxZ2RuaGlueHZhb2VpY3Bsa2JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5NjkxMTEsImV4cCI6MjA5MzU0NTExMX0.tw3Po_SURv7ycAIkT9sK9XnF9ioVoLUHCSDh8wJrDbA';
-const ADMIN_PASSWORD = 'psgemelli2026';
+const ADMIN_PASSWORD = 'Destinazione26';
 
 const _supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
